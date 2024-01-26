b9aadf64a57588748361eb5eac8fcc7b6b783d9f
sohimahamid <156577491+sohimahamid@users.noreply.github.com>
2024-01-26 21:09:20 +0500

    Add files via upload

---
+++ py assignment by sh...py
@@ -0,0 +1,11 @@
+sub1 = int(input("math: "))
+sub2 = int(input("physics: "))
+sub3 = int(input("chemistry: "))
+sub4 = int(input("biology:  "))
+sub5 = int(input("english:  "))
+sub6 = int(input("urdu:  "))
+print()
+total = sub1+sub2+sub3+sub4+sub5+sub6
+print("Total Score: ",total)
+percentage = (total/500)*100
+print("Percentage= ",percentage,"%")
---
+++ result...py
@@ -0,0 +1,13 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/mtaus/Desktop/py assignment by sh...py
+math: 90
+physics: 67
+chemistry: 89
+biology:  56
+english:  78
+urdu:  49
+
+Total Score:  429
+Percentage=  85.8 %
